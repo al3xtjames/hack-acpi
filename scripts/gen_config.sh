a8be386b02a47c4afd976ddd85d2c3af90178a27
Alex James <theracermaster@gmail.com>
2017-02-10 17:52:58 -0600

    scripts/gen_config: Check for bdmesg

--- scripts/gen_config.sh
+++ scripts/gen_config.sh
@@ -3,18 +3,24 @@
 
 export LC_ALL=C
 
+BDMESG=$(which bdmesg)
+
 if [[ ! -e config.asl ]]; then
 	cp config_orig.asl config.asl
 fi
 
-ACPI_GNVS_ADDRESS=$(bdmesg | grep 'OperationRegion (GNVS...)' |\
-	awk -F ' ' '{gsub("addr=", ""); print $7}')
+if [ -f "$BDMESG" ]; then
+	ACPI_GNVS_ADDRESS=$($BDMESG | grep 'OperationRegion (GNVS...)' |\
+		awk -F ' ' '{gsub("addr=", ""); print $7}')
 
-if [[ -z $ACPI_GNVS_ADDRESS ]]; then
-	ACPI_GNVS_ADDRESS=0x$(bdmesg | grep 'OperationRegion(GNVS,' | tail -n 1 |\
-		awk -F ' ' '{gsub("addr=", ""); print $6}' | tr -d ',')
-fi
+	if [[ -z $ACPI_GNVS_ADDRESS ]]; then
+		ACPI_GNVS_ADDRESS=0x$($BDMESG | grep 'OperationRegion(GNVS,' | tail -n 1 |\
+			awk -F ' ' '{gsub("addr=", ""); print $6}' | tr -d ',')
+	fi
 
-if [[ ! -z $ACPI_GNVS_ADDRESS ]]; then
-	sed -i '' "s/0xDEADBEEF/$ACPI_GNVS_ADDRESS/g" config.asl
+	if [[ $ACPI_GNVS_ADDRESS != "0x" ]]; then
+		sed -i '' "s/0xDEADBEEF/$ACPI_GNVS_ADDRESS/g" config.asl
+	fi
+else
+	echo "gen_config: bdmesg not found, using fallback GNVS address"
 fi
